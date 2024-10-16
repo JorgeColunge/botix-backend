81052b0a5ced233bcb51873fb9141c5719b674e4
Jorge Colunge <cto.axiomarobotics@gmail.com>
2024-10-16 12:27:55 -0500

    correccion envio de texto

--- src/handlers/repliesHandler.js
+++ src/handlers/repliesHandler.js
@@ -218,7 +218,7 @@ export async function sendTextMessage(io, req, res) {
       break;
   
     default:
-       await WhatsAppMessageSend(io, res, phone, messageText, conversationId)
+       await WhatsAppMessageSend(io, res, phone, messageText, conversation_id)
       break;
   }
 }

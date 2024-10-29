8070aa1e42ac711d8c73e7248667bbe5e55594c5
willz4321 <willz4321@gmail.com>
2024-10-29 17:00:10 -0300

    get y set token firebase

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,8 @@ import { register as registerAuth, login as loginAuth, edit, registerUser, regis
 import { registerValidation, loginValidation } from '../validations/userValidation.js';
 import passport from 'passport';
 import '../config/passportConfig.js';
+import pool from '../config/dbConfig.js';
+// import pool from '../config/dbConfig.js';
 
 const router = express.Router();
 
@@ -16,6 +18,46 @@ router.post('/login', (req, res) => {
   const { error } = loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   loginAuth(req, res);
+});
+
+router.get('/get_token_firebase', async (req, res) => {
+  const { id_usuario } = req.query; // Cambiado de req.params a req.query
+  console.log("id de usuario", id_usuario)
+  try {
+      const result = await pool.query('SELECT token_firebase FROM users WHERE id_usuario = $1', [id_usuario]);
+      if (result.rows.length > 0) {
+          res.json(result.rows[0]);
+      } else {
+          res.status(400).send('Usuario no encontrado');
+      }
+  } catch (error) {
+      console.error('Error al obtener token de firebase:', error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+router.post('/set_token_firebase', async (req, res) => {
+  const { id_usuario, token } = req.body; // Obtener el id_usuario y el token del cuerpo de la solicitud
+
+  console.log("id de usuario", id_usuario);
+  console.log("token", token); // Opcional: Para depuración
+
+  try {
+      // Verificar si el usuario existe
+      const userResult = await pool.query('SELECT * FROM users WHERE id_usuario = $1', [id_usuario]);
+
+      if (userResult.rows.length === 0) {
+          return res.status(400).send('Usuario no encontrado'); // Usuario no existe
+      }
+
+      // Actualizar el token_firebase del usuario
+      await pool.query('UPDATE users SET token_firebase = $1 WHERE id_usuario = $2', [token, id_usuario]);
+
+      res.status(200).send('Token de Firebase actualizado correctamente'); // Confirmación de éxito
+  } catch (error) {
+      console.error('Error al actualizar el token de firebase:', error);
+      res.status(500).send('Internal Server Error'); // Manejo de errores
+  }
 });
 
 // Ruta para actualizar un usuario

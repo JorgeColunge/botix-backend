c01ba3a69fffe954dbd7d6798c0288d1ff1c4ebe
willz4321 <willz4321@gmail.com>
2025-01-18 19:35:37 -0300

    user privileges

--- src/models/userPrivileges.js
+++ src/models/userPrivileges.js
@@ -1,35 +1,32 @@
 import { DataTypes } from 'sequelize';
 
 const defineUserPrivileges = (sequelize) => {
-  const UserPrivileges = sequelize.define('UserPrivileges', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    userId: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'id_usuario',
+    const UserPrivileges = sequelize.define('UserPrivileges', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      onDelete: 'CASCADE', // Eliminar solo la relación si el usuario es eliminado
-      onUpdate: 'CASCADE', // Si se actualiza el usuario, actualizar la relación también
-    },
-    privilegeId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'Privileges',
-        key: 'id',
+      userId: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
       },
-      onDelete: 'RESTRICT', // No se eliminará el privilegio si el usuario se elimina, solo se elimina la relación
-      onUpdate: 'CASCADE', // Si se actualiza el privilegio, actualizar la relación también
-    },
-  });
-
-  return UserPrivileges;
-};
-
-export default defineUserPrivileges;
+      privilegeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Privileges',
+          key: 'id',
+        },
+      },
+    });
+  
+    return UserPrivileges;
+  };
+  
+  export default defineUserPrivileges;
+  
